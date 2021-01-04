fb8ef5a3bc0f87c509ba9d8e872d1f0d3255726f
n7-remax <ditritusa@gmail.com>
2021-01-05 00:23:01 +0200

    rewrite Diary component

--- src/components/Diary/Diary.tsx
+++ src/components/Diary/Diary.tsx
@@ -1,13 +1,20 @@
-import { Component } from "react";
+import { useState } from "react";
+import DateTimePicker from "react-datetime-picker";
 
-class Diary extends Component {
-    render() {
-        return (
-            <div className="diary">Diary</div>
-
-        )
-    }
-}
-
+const Diary = () => {
+  const [date, onDateChange] = useState(new Date());
+  console.log(date);
+  return (
+    <div className="diary">
+      <DateTimePicker
+        value={date}
+        onChange={onDateChange}
+        isCalendarOpen={true}
+        maxDetail="hour"
+        locale="en-EN"
+      />
+    </div>
+  );
+};
 
 export default Diary;
